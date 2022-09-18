8428c72416345d0e2557145b6582b5eba79e8b28
Amy Gardiner <amymgardiner@gmail.com>
2022-09-18 14:22:56 -0500

    created add reaction and remove reaction functions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -102,6 +102,34 @@ const thoughtController = {
       })
       .catch((err) => res.json(err));
   },
+
+  // add a reaction
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $addToSet: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought with this id' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  // delete a reaction
+  removeReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.json(err));
+  },
 };
 
 module.exports = thoughtController;
